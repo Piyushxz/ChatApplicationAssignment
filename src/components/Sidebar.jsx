383b432326a56cfd1b4eee14563b07aed114728d
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-10-14 22:10:10 +0530

    Structure

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import CssBaseline from '@mui/material/CssBaseline';
+import AlignItemsList from './Users';
+
+const drawerWidth = 280; 
+
+export default function PersistentDrawerLeft() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      
+   
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar sx={{backgroundColor:"black"}}>
+        <Typography
+            sx={{
+                zIndex: 1,
+                mt: 1,
+                mb: 2,
+                pl: 2,
+                fontFamily: 'Montserrat',
+                fontWeight: 900,
+                fontSize: '2.5rem',
+                background: 'linear-gradient(to right, #2dd4bf, #3b82f6)', 
+                WebkitBackgroundClip: 'text', 
+                backgroundClip: 'text', 
+                color: 'transparent', 
+            }}
+  variant="h6"
+  noWrap
+  component="div"
+>
+  Whisper
+</Typography>
+
+        </Toolbar>
+      </AppBar>
+      
+
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          height:"100vh",
+         
+          zIndex: 10,
+          [`& .MuiDrawer-paper`]: { 
+            width: drawerWidth, 
+            boxSizing: 'border-box', 
+            mt: '-64px', 
+          },
+          
+        }}
+      >
+        <Toolbar /> 
+        <Box sx={{ overflow: 'auto' }}>
+          <Typography 
+            variant="h6" 
+            
+            sx={{mt: 0, p:2, fontFamily: 'Montserrat', fontWeight: '900',fontSize:"2.1rem" }}
+          >
+            Chats
+          </Typography>
+          <Divider />
+          <List>
+            <AlignItemsList />
+          </List>
+        </Box>
+      </Drawer>
+      
+
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: '#191919', p: 3, width: `calc(100% - ${drawerWidth}px)` ,height:"100vh"}}
+      >
+        <Toolbar /> 
+        <Typography paragraph>
+          Welcome to the chat interface. This is where the chat window will be.
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/components/Users.jsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+export default function AlignItemsList() {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem alignItems="center">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                Ali Connors
+              </Typography>
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider />
+      <ListItem alignItems="center">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                Ali Connors
+              </Typography>
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider />
+      <ListItem alignItems="center">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                Ali Connors
+              </Typography>
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider />
+    </List>
+  );
+}
